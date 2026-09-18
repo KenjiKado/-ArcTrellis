acdc42bbf6553e97a35098e60876907d05aaa279
Alina <kenji.kado@yandex.ru>
2026-09-18 16:53:06 +0300

    Import project service in scene chapter smoke check

--- src/ArcTrellis.App/SceneChapterDrag.cs
+++ src/ArcTrellis.App/SceneChapterDrag.cs
@@ -6,6 +6,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 using ArcTrellis.Core.Models;
+using ArcTrellis.Core.Services;
 
 namespace ArcTrellis.App;
 
